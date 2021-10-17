9410a9fa727700eba190dd0e1ce0db15201eb7a0
Iago Freitas <iagofreitas05@gmail.com>
2021-10-17 19:06:20 -0300

    1 - Fluxo de cancelamento de compra

--- src/main/java/main/place/repository/OrderItemRepository.java
+++ src/main/java/main/place/repository/OrderItemRepository.java
@@ -1,10 +1,12 @@
 package main.place.repository;
 
 import main.place.entity.OrderItem;
+import main.place.entity.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
     public List<OrderItem> findByIdPedido(Integer id);
+    public List<OrderItem> findOrderItemByStatus(String status);
 }
--- src/main/java/main/place/rest/OrderItemController.java
+++ src/main/java/main/place/rest/OrderItemController.java
@@ -2,13 +2,18 @@ package main.place.rest;
 
 import lombok.RequiredArgsConstructor;
 import main.place.entity.Cupon;
+import main.place.entity.EntidadeDominio;
 import main.place.entity.OrderItem;
+import main.place.entity.PurchaseOrder;
 import main.place.facade.Facade;
 import main.place.repository.OrderItemRepository;
+import main.place.repository.PurchaseOrderRepository;
 import main.place.services.UserService;
+import org.hibernate.criterion.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,6 +24,7 @@ public class OrderItemController {
     private final Facade facade;
     private final UserService userService;
     private final OrderItemRepository orderItemRepository;
+    private final PurchaseOrderRepository purchaseOrderRepository;
 
     @GetMapping("cancelItem/{id}")
     @CrossOrigin
@@ -42,13 +48,57 @@ public class OrderItemController {
         facade.salvar(orderItemSaved);
     }
 
+    @GetMapping("findByStatus/{status}")
+    @CrossOrigin
+    public List<OrderItem> findByStatus(@PathVariable String status){
+        return orderItemRepository.findOrderItemByStatus(status);
+    }
+
     @GetMapping("requestCancel/{id}")
     @CrossOrigin
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void requestCancel(@PathVariable Integer id){
         Optional<OrderItem> orderItemOptional = orderItemRepository.findById(id);
         OrderItem orderItemSaved = orderItemOptional.get();
-        orderItemSaved.setStatus("cancelamento_solicitado");
+
+        Optional<PurchaseOrder> purchaseOrder = purchaseOrderRepository.findById(orderItemSaved.getIdPedido());
+        PurchaseOrder purchaseOrderSaved = purchaseOrder.get();
+
+        if(purchaseOrderSaved.getStatus().equals("andamento")){
+            cancelItem(orderItemSaved.getId());
+        }else {
+            orderItemSaved.setStatus("cancelamento_solicitado");
+            facade.salvar(orderItemSaved);
+        }
+    }
+
+    @GetMapping("aprovedCancel/{id}")
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void aprovedCancel(@PathVariable Integer id){
+        Optional<OrderItem> orderItemOptional = orderItemRepository.findById(id);
+        OrderItem orderItemSaved = orderItemOptional.get();
+        orderItemSaved.setStatus("cancelamento_aprovado");
         facade.salvar(orderItemSaved);
+    }
+
+    @GetMapping("deniedCancel/{id}")
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void daniedCancel(@PathVariable Integer id){
+        Optional<OrderItem> orderItemOptional = orderItemRepository.findById(id);
+        OrderItem orderItemSaved = orderItemOptional.get();
+        orderItemSaved.setStatus("cancelamento_negado");
+        facade.salvar(orderItemSaved);
+    }
+
+    @GetMapping("returnMade/{id}")
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void returnMade(@PathVariable Integer id, OrderItem purchaseOrder){
+        Optional<EntidadeDominio> entidadeDominio =  facade.consultar(id, purchaseOrder);
+        OrderItem purchaseOrderSaved = (OrderItem) entidadeDominio.get();
+        purchaseOrderSaved.setShippingStatus("retorno_enviado");
+        facade.alterar(purchaseOrderSaved);
     }
 }
